a3f60031f47e788678329d0b8b45ee8be67d1882
yLivi <ylivia07@gmail.com>
2024-10-13 16:15:21 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,47 @@
+/*
+   OBJETIVO - quando clicar no pokedev da listagem temos que esconder o cartão do pokedev aberto 
+   e mostrar o cartão correspondente ao que foi selecionado na listagem
+       PASSO 1 - precisamos criar uma variável no JS pra trabalhar com a listagem de pokedevs
+       PASSO 2 - Identificar o evento de clique no elemento da listagem
+       PASSO 3 - remover a classe aberto só do cartão que tá aberto
+       PASSO 4 - ao clicar em um pokedev da lista pegamos o id desse pokedev pra saber qual cartão abrir
+       PASSO 5 - remover a classe ativo que marca o pokedev selecionado na listagem
+       PASSO 6 - adicinar a classe ativo no pokedev selecionado na listagem
+*/
+
+const listaSelecaoPokedevs = document.querySelectorAll(".pokedev");
+
+listaSelecaoPokedevs.forEach(pokedev =>{
+    pokedev.addEventListener("click", () => {      
+        esconderCartaoPokedev();
+
+        const idPokedevSelecionado = mostrarCartaoPokedevSelecionado(pokedev);
+
+        desativarPokedevNaListagem();
+        ativarPokedevSelecionadoNaListagem(idPokedevSelecionado);
+
+    })
+})
+
+function ativarPokedevSelecionadoNaListagem(idPokedevSelecionado) {
+    const pokedevSelecionadoNaListagem = document.getElementById(idPokedevSelecionado);
+    pokedevSelecionadoNaListagem.classList.add("ativo");
+}
+
+function desativarPokedevNaListagem() {
+    const pokedevAtivoNaListagem = document.querySelector(".ativo");
+    pokedevAtivoNaListagem.classList.remove("ativo");
+}
+
+function mostrarCartaoPokedevSelecionado(pokedev) {
+    const idPokedevSelecionado = pokedev.attributes.id.value;
+    const idDoCartaoPokedevParaAbrir = "cartao-" + idPokedevSelecionado;
+    const CartaoPokedevParaAbrir = document.getElementById(idDoCartaoPokedevParaAbrir);
+    CartaoPokedevParaAbrir.classList.add("aberto");
+    return idPokedevSelecionado;
+}
+
+function esconderCartaoPokedev() {
+    const cartaoPokedevAberto = document.querySelector(".aberto");
+    cartaoPokedevAberto.classList.remove("aberto");
+}
